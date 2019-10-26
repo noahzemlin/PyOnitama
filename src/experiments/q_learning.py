74139018a01ace3e21ae2b10978aeb92b3038b7a
Noah Zemlin <noah.zemlin@gmail.com>
2019-10-25 21:22:11 -0500

    exhibition matches

--- src/experiments/q_learning.py
+++ src/experiments/q_learning.py
@@ -12,6 +12,9 @@ class QLearningExperiment(BaseExperiment):
 
         # Eventually move to using the same agent for both, but difficult to get last action
         self.q_agent = QLearningAgent()
+        self.stored_epsilon = self.q_agent.epsilon
+        self.stored_alpha = self.q_agent.alpha
+        self.random_agent = RandomAgent()
 
         self.blue_agent = self.q_agent
         self.red_agent = self.q_agent
@@ -19,21 +22,36 @@ class QLearningExperiment(BaseExperiment):
         self.do_render = False
 
         self.num_games = 0
+        self.wins = 0
 
     def game_ended(self, game_state: GameState):
         self.num_games += 1
 
-        if self.num_games < 1000000:
-            if self.num_games % 10000 == 0:  # every 10000 games, record and reset agents
-                print(f'{self.num_games / 1000000.0 * 100.0}% done!')
+        if self.num_games < 200000:
+            if self.num_games % 2500 == 2000:  # every 10000 games, record and reset agents
+                print(f'{self.num_games / 200000.0 * 100.0}% done!')
                 with open("knowledge.txt", 'w') as f:
                     json.dump(self.q_agent.Q, f)
 
-            # swap positions to learn both sides
-            temp = self.blue_agent
-            self.blue_agent = self.red_agent
-            self.red_agent = temp
+                # perform exhibitionism
 
+                self.blue_agent = self.random_agent
+                self.red_agent = self.q_agent
+                self.q_agent.epsilon = 0
+                self.q_agent.alpha = 0
+                self.wins = 0
+            elif self.num_games % 2500 > 2000 and self.num_games % 2500 != 2499:  # exhibition matches
+                if game_state.winner == self.q_agent.last_played:
+                    self.wins += 1
+                temp = self.blue_agent
+                self.blue_agent = self.red_agent
+                self.red_agent = temp
+            elif self.num_games % 2500 == 2499:  # exhibition done
+                print(f'wins {self.wins / 500}')
+                self.q_agent.epsilon = self.stored_epsilon
+                self.q_agent.alpha = self.stored_alpha
+                self.blue_agent = self.q_agent
+                self.red_agent = self.q_agent
             return True
         else:
             # Store agent's Q into file
